6263a9d0970b8b2a68134033391be44cd525f64b
hassnaeoum <oumezdii@gmai.com>
2023-11-22 16:14:23 -0800

    clone

---
+++ setup_mysql_dev.s
@@ -0,0 +1,11 @@
+-- prepares MySQL server for the current project:
+-- create new user
+-- Create database if not exists
+-- grant all privileges on the database hbnb_dev_db
+-- grant SELECT privilege on the database performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
